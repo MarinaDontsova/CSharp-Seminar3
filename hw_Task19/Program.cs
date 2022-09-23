bc954097dc95c53dc2a5989196bc217344cf0cc7
Марина Донцова <dontsovam@mail.ru>
2022-09-23 14:07:55 +0300

    Решила задачу № 19 - проверка, является ли число палиндромом

--- hw_Task19/Program.cs
+++ hw_Task19/Program.cs
@@ -6,9 +6,26 @@
 // 23432 -> да
 
 Console.WriteLine("Введите пятизначное число: ");
-int number = Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine());
+int tempNumber = number;
+int reversedNumber = 0;
 
-if ((number % 1000) >= 0)
+if (number >= 10000 && number <= 99999)
 {
-    
+    while(tempNumber > 0)
+    {
+        int digit = tempNumber % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        tempNumber = tempNumber / 10;
+    }
+    Console.WriteLine($"Зеркальное число - {reversedNumber}");
+    if (reversedNumber == number)
+    {
+        Console.WriteLine("Введенное число является палиндромом.");
+    }
+    else Console.WriteLine("Введенное число не является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Введенное число не является 5-ти значным. Введите пятизначное число:");
 }
